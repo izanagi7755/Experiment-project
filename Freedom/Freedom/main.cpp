f11e204093874a1597abde4fb84e0fa45c46dfe6
izanagi7755 <izanagi7755@gmail.com>
2020-02-23 14:28:51 +0900

    main.hの追加（何も書いてない） 10×10 の■を生成。

--- Freedom/Freedom/main.cpp
+++ Freedom/Freedom/main.cpp
@@ -7,4 +7,13 @@ using namespace std;
 int main()
 {
     cout << "テスト用！！！" << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << "■";
+		}
+		cout << endl;
+	}
 }
